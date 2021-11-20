4f6f5d358c67aafe801779110a0f7032042a4503
YFarias <yurisfarias@gmail.com>
2021-11-20 14:56:34 +0000

    mudanças - game route, view game

--- routes/game.routes.js
+++ routes/game.routes.js
@@ -1,107 +1,48 @@
-const express = require("express").Router
+const express = require('express');
 const router = express.Router();
+
 const User = require("../models/User.model");
 const Game = require("../models/Game.model");
 
 
 //!Game Routes
 
-//Create new Movies
-router.get('/game', (req, res, next) => {
-    Game.find()
-    .then( gameListDB => {
-      res.render('/views/games/searchgame.hbs', { gameListDB });
-    })
-    .catch( err => console.log("Error while displaying a form to create a games: ", err))
-  })
-  
-  // post route to create movies
-  {/* <form action="/movies" method="POST"> */}
-  
-  router.post('/games', (req, res, next) => {
+//Create new Game
+router.post("/games", (req, res) => {
     Game.create(req.body)
-    .then( gameListDB => {
-      // console.log("New movie: ", newMovie);
+      .then((games) => res.render("/views/games/addgame.hbs",{ data: games }))
+      .catch((error) => console.log(error));
+  });
   
-      // res.redirect ALWAYS has '/' because it is the URL
-      res.redirect('/games')
-    } )
-    .catch( err => console.log("Error while creating a game: ", err))
-  })
+  // GET /games
+  router.get("/games", (req, res) => {
+    Game.find()
+      .then((games) => res.render("/views/games/gamelist.hbs",{ data: games }))
+      .catch((error) => console.log(error));
+  });
   
-  // get all the movies from the DB
+  // GET /game find by id
+  router.get("/views/games/gamelist.hbs", (req, res) => {
+    Game.findOne({ _id: req.params.gameId })
+      .then((game) => res.render({ data: game }))
+      .catch((error) => console.log(error));
+  });
   
-  route.get('/', (req, res, next) => {
-    Movie.find()
-    .then( moviesFromDB => {
-      res.render('movies/movies', { moviesFromDB });
-    })
-    .catch( err => console.log("Error while displaying all the movies: ", err))
-  })
+  // POST /game/gameId
+  router.post("/views/games/gamelist/:gamesId", (req, res) => {
+    Album.findOneAndUpdate({ _id: req.params.gamesId }, req.body, { new: true })
+      .then((gameId) => res.json({ data: game }))
+      .catch((error) => console.log(error));
+  });
   
-  // delete route:
-  {/* <form action="/movies/{{this._id}}/delete" method="POST"> */}
-  movieRouter.post('/:id/delete', (req, res, next) => {
-    Movie.findByIdAndRemove(req.params.id)
-    .then(() => {
-      res.redirect('/movies')
-    })
-    .catch( err => console.log("Error while deleting a movie: ", err))
-  })
+  // POST /games/:gamesId/delete
+  router.post("/views/games/gamelist/:gamesId/delete", (req, res) => {
+    Album.findByIdAndDelete(req.params.gameId)
+      .then(() => res.sendStatus(204))
+      .catch((error) => console.log(error));
+  });
   
-  // edit the movie -get route to display the form
-  {/* <a href="/movies/{{theMovie._id}}/edit">Edit the Movie</a> */}
-  movieRouter.get('/:id/edit', (req, res, next) => {
-    Movie.findById(req.params.id)
-    .then(foundMovie => {
-      Celebrity.find()
-      .then(allCelebs => {
-        allCelebs.forEach(oneCeleb => {
-          foundMovie.cast.forEach(oneCastMember => {
-            // console.log("what is this: ", oneCastMember);
-            if(oneCeleb._id.equals(oneCastMember)){
-              oneCeleb.isInCast = true;
-            }
-          })
-        })
-        res.render('movies/edit-movie', { movie: foundMovie, allCelebs })
-      })
-    })
-    .catch( err => console.log("Error while getting the movie for the edit form: ", err))
-  })
-  
-  // post route to save the updates in the movie
-  {/* <form action="/movies/{{movie._id}}/update" method="POST"> */}
-  movieRouter.post('/:movieId/update', (req, res, next) => {
-    // we can use 'req.body' since we used the same names as in our MOVIE model
-    Movie.findByIdAndUpdate(req.params.movieId, req.body)
-    .then( updatedMovie => {
-      // if everything is fine, take me back to the details page so we can see the changes we made
-      res.redirect(`/movies/${req.params.movieId}`);
-    } )
-    .catch( err => console.log("Error while getting updating the movie: ", err))
-  })
-  
-  
-  
-  
-  
-  
-  // details page:
-    // movieId is a placeholder, can be any name
-    movieRouter.get('/:movieId', (req, res, next) => {
-      // cast is the array of IDs and we need full object so we need to use
-      // .populate('cast) and pass the "cast" in the method because we are populating that specific field
-      Movie.findById(req.params.movieId).populate('cast')
-      .then( theMovie => {
-        res.render('movies/movie-details', { theMovie })
-      } )
-      .catch( err => console.log("Error while displaying movie details: ", err))
-  })
-  
-  module.exports = movieRouter;
-
-/*Game List*/
+  module.exports = router;
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,11 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
+//Routes
+
+//games routes
+const gamesRouter = require('../routes/game.routes');
+app.use('/', gamesRouter);
+
+
 module.exports = router;
