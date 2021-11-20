75ba0de35e4f5ad9c4688c310ba092c43eb47877
YFarias <yurisfarias@gmail.com>
2021-11-20 12:30:46 +0000

    game model

--- models/Game.model.js
+++ models/Game.model.js
@@ -1,20 +1,19 @@
 const { Schema, model } = require("mongoose");
 
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
-const userSchema = new Schema(
+const gameSchema = new Schema(
   {
-    username: {
-      type: String,
-      // unique: true -> Ideally, should be unique, but its up to you
-    },
-    password: String,
-  },
-  {
-    // this second object adds extra properties: `createdAt` and `updatedAt`
-    timestamps: true,
+    name: {type:String, require:true},
+    category: {
+      type:String,
+      enum:["Action","Adventure","War","Strategy","Sports",
+      "RPG","Race","Simulation"]},
+    url: {type: String},
+    username: {type: [mongoose.type.Schema.ObjectId],  ref: 'User'},
+  
   }
 );
 
-const User = model("User", userSchema);
+const Game = model("Game", gameSchema);
 
-module.exports = User;
+module.exports = Game;
