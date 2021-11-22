893b040ae105e9e2f295394db6a9a4d0accd6808
fabioFG <fabio.fernando.guerreiro@gmail.com>
2021-11-22 23:56:40 +0000

    add search-bar and search function, remove view, change layout

--- routes/game.routes.js
+++ routes/game.routes.js
@@ -10,17 +10,17 @@ const Game = require("../models/Game.model");
 
 //Game search 
 
-/* router.get("/search", (req, res) => {
+router.get("/search", (req, res) => {
   const gameTitle = req.query.gameTitle; 
 
-  //Finding the game we want in a more fluid way    {title: { $regex: gameTitle, $option: "i"} }
-  Game.find( {title: { $regex: gameTitle, $options: "i"} })
+  //Finding the game we want in a more fluid way 
+  Game.find({title: { $regex: gameTitle, $options: "i"} })
   .then((gameList) => { //array with games found 
     res.render("games/gamelist", {gameList: gameList})
   })
 
   .catch((err) => console.log(err));
-}) */
+}) 
 
 
 
