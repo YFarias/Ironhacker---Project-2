0ff3f29d0e3f22031b40f4b672860896aa3769c2
YFarias <yurisfarias@gmail.com>
2021-11-22 15:27:51 +0000

    logout - falta criar a session no db

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,19 @@ const isLoggedIn = require("./../middleware/isLoggedIn"); //imported this to hav
 //Homepage route
 /* GET home page */
 router.get("/", (req, res, next) => {
-  res.render("index");
+  // Check if the incoming request has a valid cookie/session
+
+  let userIsLoggedIn = false;
+  if (req.session.user) {
+    userIsLoggedIn = true;
+  }
+
+  res.render("/index", { userIsLoggedIn: userIsLoggedIn });
 });
 
 
 //games routes
 const gamesRouter = require('../routes/game.routes');
-//app.use('/', gamesRouter); // what is this? its not defined and giving an error
-
-
 
 //Private routes
 // Get /private
