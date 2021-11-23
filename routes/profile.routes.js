ca53ba81071f4be75915d78cbc328af886c90203
YFarias <yurisfarias@gmail.com>
2021-11-23 18:23:44 +0000

    profile - Routes, edit.hbs and profile,hbs

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,22 +1,61 @@
 const router = require("express").Router();
 const isLoggedIn = require("./../middleware/isLoggedIn"); //imported this to have the logged in session
-
-
-
+const User = require("./../models/User.model");
 
 
 // Get /private profile view
-router.get("/profile", isLoggedIn, (req,res,next) =>{
+router.get("/profile", isLoggedIn, async (req,res,next) =>{
 
     //If the user is still logged the layout with profile and logout still show
     let userIsLoggedIn = false;
     if(req.session.user) {
       userIsLoggedIn = true
     }
-    res.render("profile", { userIsLoggedIn: userIsLoggedIn });
+    const userID = req.session.user._id
+    console.log('userID :', userID)
+    const user = await User.findById(userID)
+    res.render("profile", { userIsLoggedIn: userIsLoggedIn, user });
     
   });
 
 
   //EDIT PROFILE
+  router.get("/edit", isLoggedIn, (req, res)=>{
+   /*    User.findById(req.params.id) */
+
+    res.render("users/edit-profile", { user: req.session.user})
+
+  });
+
+  router.post("/edit", (req,res)=> {
+   
+    const userID = req.session.user._id
+    
+    const {username,password} = req.body
+   
+    User.findByIdAndUpdate(userID, {username: username,password:password} )
+    .then((data) => {
+        console.log('data :', data)
+    res.redirect("/profile")})
+    .catch((error) => console.log(error));
+});
+            
+            
+            
+        
+
+
+
+   
+   
+    /* get the new info from the body
+      find user and update
+      redirect to user profile
+   */
+
+
+
+
+
+
   module.exports = router;
